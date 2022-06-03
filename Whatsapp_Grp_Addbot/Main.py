3e8d0fc464646d41d0f17024bbf2b280fc4f0b73
Harshit Khandwalia <harshit8377@gmail.com>
2022-06-03 22:36:46 +0530

    Whatsapp_Grp_Addbot

---
+++ Whatsapp_Grp_Addbot/Main.py
@@ -0,0 +1,41 @@
+import pyautogui as pt
+import csv
+from time import sleep
+
+sleep(3)   # Delay time given to open whatsapp app
+
+def addpeople():
+
+
+    pt.click(523, 49, duration=0.5) # Used to click on the whatsapp group name
+    pt.moveTo(1000, 200, duration=0.5)  # Moves to right column where you have to scroll
+    pt.scroll(-500)   # Scrolls the column
+    pt.click(1047, 612, duration=0.5) # Clicks on "add participant"
+    sleep(1) # Delay
+
+
+
+    # Now Adding Participants
+    # Reading CSV file
+    l = []
+    with open('check.csv', 'rt') as f:  # Opens the csv file where all the contacts with their name is already added
+         data = csv.reader(f)
+         for row in data:
+             if row[1] != "Given Name":
+                 l.append(row[1])
+
+    for i in l:
+        sleep(0.2)
+        pt.typewrite(i)    # Type the name of participant in blank
+        sleep(0.2)   # Delay after writing the name
+        pt.click(639,377,duration=0.1)  # Select the participant
+        pt.click(861, 213, duration=0.1) # Select the cross on the participant blank
+                                        # Loop ends after adding all the participants
+    pt.click(823, 553, duration=0.1)  # checks the tick button
+    pt.click(758, 415, duration=0.1)  # clicks "Add Participant" button
+    # All the participants are added
+
+addpeople()
+#program ends
+
+
---
+++ Whatsapp_Grp_Addbot/Readme.txt
@@ -0,0 +1,20 @@
+# Whatsapp_Grp_Addbot
+ABOUT: This python code helps in adding many participants to a whatsapp group instantly ,it controls the mouse pointer to do so ,it can add as many members as you want it to !!
+
+PRE-REQUISITES PAKAGES are:-
+Opencv,
+Pyautogui,
+Time,
+CSV
+
+
+PROCEDURE:-
+
+1) To run this program efficiently ,you have to see whether you have the participants in your phonebook directory or not
+2) If they already exist in the phone book directory ,you just have to make a CSV file containing participant's name and contact and just link it with the code 
+3) Else, you have to make the csv and then import the csv to google conatcts to add all the people to contact list and then repeat step (2)
+4) Now the contact part is done, now you have to adjust the clicks according to your screen size 
+5) Position.py is given ,through which you can track the coordinates of mouse pointer,if the code doesn't work well for your scrren size,adjust it yourself with the help of position code 
+
+
+# THAT'S IT ,HOPE THE CODE RUNS FOR YOU TOO!!
